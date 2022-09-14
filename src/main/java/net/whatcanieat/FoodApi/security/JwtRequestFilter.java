80cb189dae7705e9cbfcc4afb3a3e65a2b603856
Cr1o <sandeclopbautista@gmail.com>
2022-09-14 19:51:38 -0300

    Enabled web config

--- src/main/java/net/whatcanieat/FoodApi/security/JwtRequestFilter.java
+++ src/main/java/net/whatcanieat/FoodApi/security/JwtRequestFilter.java
@@ -3,6 +3,7 @@ package net.whatcanieat.FoodApi.security;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JwtRequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
--- src/main/java/net/whatcanieat/FoodApi/security/SecurityConfig.java
+++ src/main/java/net/whatcanieat/FoodApi/security/SecurityConfig.java
@@ -2,22 +2,32 @@ package net.whatcanieat.FoodApi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtRequestFilter requestFilter;
+
     @Autowired
-    JwtRequestFilter requestFilter;
+    public SecurityConfig(JwtRequestFilter requestFilter) {
+        this.requestFilter = requestFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http    .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
+                    .antMatchers("/").permitAll()
                     .antMatchers("*/abm/*").authenticated()
                     .anyRequest().permitAll()
                 .and()
