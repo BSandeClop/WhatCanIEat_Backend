de9ba4eec2b1484b6fffc219e3692ee5ca7266ea
Cr1o <sandeclopbautista@gmail.com>
2022-09-14 19:45:16 -0300

    Se cambio la clase de seguridad

--- src/main/java/net/whatcanieat/FoodApi/security/SecurityConfig.java
+++ src/main/java/net/whatcanieat/FoodApi/security/SecurityConfig.java
@@ -3,26 +3,29 @@ package net.whatcanieat.FoodApi.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     JwtRequestFilter requestFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http    .csrf().disable()
-                .authorizeRequests(
-                        (auth) -> auth
-                                .antMatchers("*/abm/*").authenticated()
-                                .anyRequest().permitAll()
-                )
-                .formLogin().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .logout().disable();
+        http    .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("*/abm/*").authenticated()
+                    .anyRequest().permitAll()
+                .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .formLogin().disable()
+                    .logout().disable();
 
         http.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
 
